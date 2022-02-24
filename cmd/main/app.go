468c766e2e3b562366a95b9aedcbbdef63e3e0b2
Oybek <programmingiswonderful@gmail.com>
2022-02-24 16:36:49 +0500

    working with application configurations

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
+	"restful_go_project/internal/config"
 	"restful_go_project/internal/user"
 	"restful_go_project/pkg/logging"
 	"time"
@@ -17,14 +20,25 @@ func main() {
 	logger.Info("create router")
 	router := httprouter.New()
 
+	cfg := config.GetConfig()
+
 	handler := user.NewHandler(logger)
 	handler.Register(router)
 
-	start(router)
+	start(router, cfg)
 }
 
-func start(router *httprouter.Router) {
-	listener, err := net.Listen("tcp", ":1234")
+func start(router *httprouter.Router, cfg *config.Config) {
+	logger := logging.GetLogger()
+	logger.Info("start application")
+
+	if cfg.Listen.Type == "sock" {
+		filepath.Abs(filepath.Dir(os.Args[0]))
+	} else {
+
+	}
+
+	listener, err := net.Listen("tcp", cfg.Listen.Port)
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module restful_go_project
 go 1.13
 
 require (
+	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.2.6 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/sirupsen/logrus v1.8.1
 )
