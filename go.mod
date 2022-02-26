8b94fa718e3b58a56a57f98c521c6865c3aaae9a
Oybek <programmingiswonderful@gmail.com>
2022-02-26 21:52:05 +0500

    mongo client and user model done

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/sirupsen/logrus v1.8.1
+	go.mongodb.org/mongo-driver v1.8.3
 )
---
+++ pkg/client/mongodb/mongodb.go
@@ -0,0 +1,46 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewClient(ctx context.Context, host, port, username, password, database, authDB string) (*mongo.Database, error) {
+	// mongoDBURL := "mongodb+srv://oybek:1102123112@cluster0.gs3vf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+	var mongoDBURL string
+	var isAuth bool
+
+	if username == "" && password == "" {
+		isAuth = false
+		mongoDBURL = fmt.Sprintf("mongodb://%s:%s", host, port)
+	} else {
+		isAuth = true
+		mongoDBURL = fmt.Sprintf("mongo://%s:%s@%s:%s", username, password, host, port)
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoDBURL)
+	if isAuth {
+		if authDB == "" {
+			authDB = database
+		}
+		clientOptions.SetAuth(options.Credential{
+			AuthSource: authDB,
+			Username:   username,
+			Password:   password,
+		})
+	}
+
+	cleint, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("field to connect to MongoDB due to error: %v", err)
+	}
+
+	if err = cleint.Ping(ctx, nil); err != nil {
+		return nil, fmt.Errorf("field to ping to MongoDB due to error: %v", err)
+	}
+
+	return cleint.Database(database), nil
+}
