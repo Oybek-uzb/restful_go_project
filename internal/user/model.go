3d19ff5ef312cbb6287b45a5e59dfb14f0ef5690
Oybek <programmingiswonderful@gmail.com>
2022-02-26 21:59:13 +0500

    models modefied and storage interface done

--- internal/user/model.go
+++ internal/user/model.go
@@ -2,7 +2,13 @@ package user
 
 type User struct {
 	ID           string `json:"id" bson:"_id, omitempty"`
+	Email        string `json:"email" bson:"email"`
 	Username     string `json:"username" bson:"username"`
 	PasswordHash string `json:"-" bson:"password"`
-	Email        string `json:"email" bson:"email"`
+}
+
+type CreateUserDTO struct {
+	Email        string `json:"email"`
+	Username     string `json:"username"`
+	Password string `json:"password"`
 }
